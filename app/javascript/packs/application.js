8a72d51749795c99f18053fe7fcf7bfc77db472e
Pazykiv <pazykiv@ukr.net>
2021-07-29 23:12:18 +0300

    Finish do review and ratings

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,22 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("jquery")
+require("turbolinks").start()
+require("packs/jquery.raty")
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
+
+
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "../stylesheets/application"
 
-Rails.start()
+//Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-global.toastr = require("toastr")
+//global.toastr = require("toastr")
